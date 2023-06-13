9ad2dab3fa752ed4abb26ef4147650637730789b
Failfilin <apseliverstov@yandex.ru>
2023-06-13 16:47:34 +0300

    Correct work with NUllable

--- src/main/kotlin/CopyRight.kt
+++ src/main/kotlin/CopyRight.kt
@@ -1,6 +1,6 @@
 class CopyRight(
-    val id: Int?,
-    val link: String?,
-    val name: String?,
-    val type: String?
+    val id: Int?= null,
+    val link: String? = null,
+    val name: String? = null,
+    val type: String? = null
 )
